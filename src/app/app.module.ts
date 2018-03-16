9de61e82bd372595a05e2241fc503dc73ece1f37
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-03-16 12:17:04 -0400

    Nested *ngFor iterations within a single component. Initial Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
